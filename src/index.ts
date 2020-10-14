b1f5804bed9635ae32257b32421b3f810830aab4
SkyLightQP <combbm@naver.com>
2020-10-14 15:41:03 +0900

    Project init

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import cors from 'cors';
+import express from 'express';
+import log4js from 'log4js';
+
+export const app = express();
+export const logger = log4js.getLogger();
+
+log4js.configure({
+  appenders: {
+    console: {
+      type: 'console'
+    },
+    default: {
+      type: 'file',
+      filename: 'logs/sinamon.log',
+      pattern: '-yyyy-MM-dd',
+      compress: true
+    }
+  },
+  categories: {
+    default: {
+      appenders: ['default', 'console'],
+      level: 'DEBUG'
+    }
+  }
+});
+logger.level = 'ALL';
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
