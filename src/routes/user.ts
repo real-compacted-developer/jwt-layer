d432513fe3026e92675160d42a55cc77968e5aa8
kkoon9 <rndrnjs2003@gmail.com>
2020-10-26 15:48:09 +0900

    feat : get user info having token
    - token만으로 유저 정보를 가져오는 API를 구성하였다.

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,17 +1,31 @@
-import { Request, Response, Router } from 'express';
+import { Request, Response, NextFunction, Router } from 'express';
+import { IncomingHttpHeaders } from 'http';
 import axios from 'axios';
-import * as getUserInfo from '../middleware/auth';
+import ErrorResponse from '../utils/errorResponse';
+import ERROR_CODE from '../constants/errorCode';
+import jwt from '../modules/jwt';
 
 const router = Router();
 
 type SuccessResponse = {
   success: true;
-  message: string;
+  message?: string;
   data : any;
 };
 
+type FailResponse = {
+  success: false;
+  message: string;
+};
+type Decode = {
+  id: string,
+  isPremium: boolean,
+  iat: number,
+  exp: number,
+  iss: 'string'
+}
 /**
- * @api {post} /
+ * @api {get} /
  * @apiName getUserInfo
  * @apiGroup user
  *
@@ -20,20 +34,35 @@ type SuccessResponse = {
  * @apiSuccess {Boolean} success token 유효 여부
  * @apiSuccess {String} message 응답 메시지
  */
-router.route('/').post([getUserInfo], async (req: Request, res: Response) => {
+// eslint-disable-next-line consistent-return
+router.get('/', async (req: Request, res: Response, next : NextFunction) => {
+  const { authorization }: IncomingHttpHeaders = req.headers;
+  let token = 'no token';
+  if (authorization && authorization.startsWith('Bearer')) {
+    token = authorization.slice(7, authorization.length);
+  } else {
+    const responseJSON: FailResponse = {
+      success: false,
+      message: ERROR_CODE.EMPTY_TOKEN
+    };
+    res.status(404).send(responseJSON);
+  }
+  // Make sure token exists
+  if (token === 'no token') {
+    return next(new ErrorResponse(ERROR_CODE.UNAUTHORIZED, 401));
+  }
+
   try {
-    // eslint-disable-next-line no-console
-    console.log(req.decoded);
-    const { id } = req.decoded;
-    const user = await axios.get(`http://db.api.connectclass.io/user/${id}`);
+    // Verify token
+    const decoded : Decode = await jwt.verify(token);
+    const user = await axios.get(`http://db.api.connectclass.io/user/${decoded.id}`);
     const responseJSON: SuccessResponse = {
       success: true,
-      message: id,
-      data : user.data.data
+      data: user.data.data
     };
     res.status(200).send(responseJSON);
-  } catch (err) {
-    return err
+  } catch (error) {
+    return next(new ErrorResponse(ERROR_CODE.UNAUTHORIZED, 401));
   }
 });
 
